ece3a5a7060646aae64f16bef424895cc5e105e6
timeforjc <timeforjc@gmail.com>
2019-01-30 12:05:08 -0800

    Adds ToDos tab with list

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import ToDos from "./views/ToDos.vue";
+import AddItem from "./views/AddToDo.vue";
 
 Vue.use(Router);
 
@@ -21,6 +23,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/todos",
+      name: "todos",
+      component: ToDos
     }
   ]
 });
