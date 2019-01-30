6465d6fe05077143d6ee26404886498b66f5d816
jlfisher <jfishe13@calpoly.edu>
2019-01-30 15:22:37 -0800

    Update ToDos.vue
    Successfully add new items

--- client/src/views/ToDos.vue
+++ client/src/views/ToDos.vue
@@ -1,35 +1,75 @@
 <template>
 <div class="todos">
-    <ul>
-        <li v-for="(todo, index) in mytodos" v-bind:key="index">{{ todo.name }} </li>
-    </ul>
-    <button class="button" v-on:click="addTodoItem">Add</button>
+  <body>
+    <H1>My Todo List</H1>
+
+    <table>
+      <tr>
+        <th>Check</th>
+        <th>Todo Item</th>
+        <th>Due Date</th>
+        <th>Delete</th>
+      </tr>
+      <tr v-for="(todo, index) in mytodos" v-bind:key="index">
+        <td>
+          <input type="checkbox">
+        </td>
+        <td>{{ todo.name }}</td>
+        <td>{{ todo.duedate }}</td>
+        <td>
+          <button class="button">Delete</button>
+        </td>
+      </tr>
+    </table>
+    <br>
+
+    <body>
+      <form v-on:submit.prevent="onSubmit">
+        <H1>New Todo Item</H1>Name
+        <!--<input class="input" type="text" v-model="item.name">-->
+        <input type="text" v-model="item.name"/>
+        <br>Due Date
+        <input type="text" v-model="item.duedate"/>
+        <br>
+        <button class="button" v-on:click="addTodoItem">Add</button>      
+      </form>
+    </body>
+  </body>
 </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 import { Component } from "vue-property-decorator";
 @Component
 export default class ToDos extends Vue {
-    mytodos: ToDo[] = [
-        { name: "Tod one", duedate: undefined },
-        { name: "Tod two", duedate: undefined },
-        { name: "Tod three", duedate: undefined },
-    ];
-    addTodoItem() {
-        this.mytodos.push({name: `todo${new Date().getTime()}`, duedate: undefined});
-    }
+item: ToDo = {
+    name: "",
+    duedate: ""
+  };
+
+  mytodos: ToDo[] = [
+    { name: "Laundry", duedate: "1/31/19" }
+  ];
+  addTodoItem() {
+    this.mytodos.push({
+      name: this.item.name,
+      duedate: this.item.duedate
+    });
+
+  }
 }
 
-interface ToDo {
-    name: string;
-    duedate: Date | undefined;
+export interface ToDo {
+  name: string;
+  duedate: string | undefined;
 }
 </script>
 
 <style scoped>
-    table, th, td {
-        border: 1px solid black;
-    }
+table,
+th,
+td {
+  border: 1px solid black;
+}
 </style>
