c82ac150e23716b18e5f96bb9095a1710c04600b
sarahkurd <skurdogh@calpoly.edu>
2019-01-30 15:41:41 -0800

    initial

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import ToDos from "./views/TODO.vue";
 
 Vue.use(Router);
 
@@ -21,6 +22,16 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
+    },
+    {
+      path: "/todos",
+      name: "todos",
+      component: ToDos
+    },
+    // {
+    //   path: "/addtodo",
+    //   name: "addtodo",
+    //   component: AddToDo
+    // }
   ]
 });
