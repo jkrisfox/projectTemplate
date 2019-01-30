1e93fc135d4c879904d47267d0be64c615779236
gwayne9955 <gwayne9955@yahoo.com>
2019-01-29 21:04:21 -0800

    Added ToDos.vue

---
+++ client/src/views/ToDos.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Hi from todos</div>
+
+</template>
+
+<script>
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+@Component
+export class ToDos extends Vue {
+    
+}
+</script>
