207525d89bf7ef8c01adfa78b938757ec2d98f6b
english13 <panthari.s4@gmail.com>
2019-01-30 16:14:02 -0800

    git push

---
+++ client/src/ToDos.vue
@@ -0,0 +1,31 @@
+<template>
+<div class = 'todos'>
+      <div>Hi from todos</div?
+         <div v-for="(todo, index) in mytodos" v-bind:key="index">
+            <span>{{ todo.name }}</span>
+            <span>{{ todo.duedate }}</span>
+         </div>
+         <button v-on:click="addTodoItem">Add</button>
+      </div>
+   </template>
+
+<script lang = "ts">
+import Vue from 'vue'
+import { Component } from "vue property-decorator";
+@Component
+export class ToDos extends Vue {
+   mytodos: ToDo[] = [
+      { name: "Todo one", duedate: undefined },
+      { name: "todo two", duedate: undefined }
+      { name: "Todo three", duedate: undefined}
+   ];
+   addTodoItem() {
+      this.mytodos.push({name: 'todo${new Date().getTime()}', duedate: undefined})
+   }
+
+   newToDo: ToDoForm = {
+      emailAddress: "",
+      password: ""Vue};
+   }
+}
+</script>
