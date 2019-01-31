13b6a5651972567b44757877e0d50f0a33094926
mfekadu <mike.fekadu@gmail.com>
2019-01-30 16:44:32 -0800

    implemented addTodoItem textbox
    implemented addTodoItem textbox
    
    still need to style it, but functionality and input checking is there (e.g. item name is needed while duedate is optional)
    
    in the future an alert() should not be the way to handle the lack of this.item.name but rather a gentle text popup inline with the textbox

--- client/src/views/ToDos.vue
+++ client/src/views/ToDos.vue
@@ -1,51 +1,63 @@
 <template>
     <div class="todos">
-        <div> Hi from todos</div>
+        <h1> HI FROM TODOS </h1>
         <!-- <h1> My Todo List </h1> -->
         <!-- this is now a data driven component
                 that outputs anything in the todos array -->
         <div v-for="(todo, index) in mytodos" v-bind:key="index">
-            <span>{{ todo.name }}</span>
-            <span>{{ todo.duedate }}</span>
+            <span>{{ todo.name }} {{ todo.duedate }}</span>
         </div>
-        <button class="button" v-on:click="addTodoItem">Add</button>
+        <br>
+        <div>
+            <h2> ADD NEW TODO </h2> 
+            Item name: <input type="text" v-model="item.name"/>
+            <br>
+            Item Due Date: <input type="text" v-model="item.duedate"/>
+            <button class="button" v-on:click="addTodoItem">Add</button>
+        </div>
     </div>
 </template>
 
 
 
 <script lang='ts'>
-import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
 
 /* NOTE: it is useful for only Api and Client 
  * in separate windows with VS Code */
 
 /* this is creating a component */
 @Component
-// if default then 
+// if default then
 // import ToDos from './views/ToDos.vue'
 //  if not default then
 // import { ToDos } from './views/ToDos.vue'
 
 // NOTE: do npm run serve in the client dir to compile
-
 export default class ToDos extends Vue {
-// this export says, if another files imports this file, 
-// then it only gets this component
-    mytodos: ToDo[] = [
-        { name: "Tod one", duedate: undefined },
-        { name: "Tod two", duedate: undefined },
-        { name: "Tod three", duedate: undefined }
-    ];
-    addTodoItem() {
-        this.mytodos.push({name: `todo${new Date().getTime()}`, duedate: undefined});
+  // this export says, if another files imports this file,
+  // then it only gets this component
+  mytodos: ToDo[] = [
+    { name: "Tod one", duedate: undefined },
+    { name: "Tod two", duedate: undefined },
+    { name: "Tod three", duedate: new Date() }
+  ];
+  item: ToDo = {
+    name: "",
+    duedate: undefined
+  };
+  addTodoItem() {
+    console.log(this.mytodos);
+    if (this.item.name === "") {
+      return alert("please enter an item name at least");
     }
+    this.mytodos.push({ name: this.item.name, duedate: this.item.duedate });
+  }
 }
 
 interface ToDo {
-    name: string;
-    duedate: Date | undefined;
-};
-
+  name: string;
+  duedate: Date | undefined;
+}
 </script>
