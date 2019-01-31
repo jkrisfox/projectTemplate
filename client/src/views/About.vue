92d550d19d9e6df5361c28dcda25668de3c467d0
michael <michael@michaels-MacBook-Pro.local>
2019-01-30 17:31:50 -0800

    sdf:wq

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
+    <button> I am a Button</button>
     <h1>This is an about page</h1>
+    
   </div>
 </template>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+     <b-button> I am a Button</b-button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
