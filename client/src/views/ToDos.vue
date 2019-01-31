7a37a993ad81f8da590590248de834ed1a8ee322
mfekadu <mike.fekadu@gmail.com>
2019-01-30 17:10:28 -0800

    css is hard

--- client/src/views/ToDos.vue
+++ client/src/views/ToDos.vue
@@ -1,18 +1,56 @@
+<style scoped>
+.table { display: table; }
+.row { display: table-row; }
+.cell {
+  display: table-cell;
+  background-color: red;
+  padding:20; /* padding is inside the "box" */
+  margin:50; /* margin is outside the "box" */
+  border-style: solid;
+  border-color: black;
+}
+
+h1 {
+  padding:10px;
+  padding-left: 10px;
+  /* margin:10px; */
+  background-color: green;
+  font-size: 15px;
+  font-weight: bold;
+}
+</style>
+
+
 <template>
-    <div class="todos">
+    <div class="todos" style="margin-left: 20px">
         <h1> HI FROM TODOS </h1>
         <!-- <h1> My Todo List </h1> -->
         <!-- this is now a data driven component
                 that outputs anything in the todos array -->
         <div v-for="(todo, index) in mytodos" v-bind:key="index">
-            <span>{{ todo.name }} {{ todo.duedate }}</span>
+            <div class="table">
+                <div class="row">
+                    <div class="cell col1">
+                    <input type="checkbox" name="name1" id="1"><br>
+                    </div>
+                    <div class="cell col2"> <span>{{ todo.name }}</span> </div>
+                    <div class="cell col3">
+                        <span>{{ (todo.duedate) ? todo.duedate : "_".repeat(24) }}</span>
+                    </div>
+                    <div class="cell col4">
+                        <button type="button" name="btn-delete">Delete</button>
+                    </div>
+                </div>
+                </div>
+
         </div>
         <br>
         <div>
-            <h2> ADD NEW TODO </h2> 
+            <h1> ADD NEW TODO </h1> 
             Item name: <input type="text" v-model="item.name"/>
             <br>
             Item Due Date: <input type="text" v-model="item.duedate"/>
+            <br>
             <button class="button" v-on:click="addTodoItem">Add</button>
         </div>
     </div>
