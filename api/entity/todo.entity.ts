c07fe44536c8bc1631f194caccdeb7bdde80a351
Reed Bowling <reedabowling@gmail.com>
2019-02-04 19:46:13 -0800

    blah

--- api/entity/todo.entity.ts
+++ api/entity/todo.entity.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Column, Entity, Index, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "../entity"
 
@@ -20,25 +19,3 @@ export class ToDo {
     @JoinColumn()
     public user!: User;
 }
-=======
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
-import { User } from "../entity";
-@Entity()
-export class ToDo {
-  @PrimaryGeneratedColumn()
-  public id!: number;
-
-  @Column()
-  public title!: string;
-
-  @Column()
-  public complete!: boolean;
-
-  @Column()
-  public dueDate!: Date;
-
-  @OneToOne((type) => User, { cascade: true })
-  @JoinColumn()
-  public user!: User;
-}
->>>>>>> 741e3335db43397093bd5c5fd609569345cb4687
