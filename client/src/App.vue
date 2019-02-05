425c78e09634ac36f3e8ad2748809c8921d788f7
Kris Fox <jfox@calpoly.edu>
2019-02-04 17:02:04 -0800

    initial version with file upload, upload not tested, but fixed login

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,34 +3,45 @@
     <div class="navbar">
       <div class="navbar-menu">
         <div class="navbar-start">
-          <router-link class="navbar-item is-tab" to="/" exact-active-class="is-active">Home</router-link> 
+          <router-link class="navbar-item is-tab" to="/" exact-active-class="is-active">Home</router-link>
           <router-link class="navbar-item is-tab" to="/about" exact-active-class="is-active">About</router-link>
         </div>
         <div class="navbar-end">
           <div class="navbar-item">
             <div class="buttons">
-              <a class="button is-primary" v-on:click="showSignupModal()">
+              <a class="button is-primary" v-if="!isLoggedIn" v-on:click="showSignupModal()">
                 <strong>Sign up</strong>
               </a>
-              <a class="button is-light" v-on:click="showLoginModal()">
-                Log in
-              </a>
+              <router-link
+                class="button is-text"
+                v-if="isLoggedIn"
+                to="/my-profile"
+                exact-active-class="is-active"
+              >My Profile</router-link>
+              <a class="button is-light" v-if="!isLoggedIn" v-on:click="showLoginModal()">Log in</a>
+              <a class="button is-light" v-if="isLoggedIn" v-on:click="logout">Log out</a>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <router-view/>
-    <Signup v-bind:is-showing="showSignup" v-on:success="successSignup()" v-on:cancel="cancelSignup()"/>
+    <router-view class="container"/>
+    <Signup
+      v-bind:is-showing="showSignup"
+      v-on:success="successSignup()"
+      v-on:cancel="cancelSignup()"
+    />
     <Login v-bind:is-showing="showLogin" v-on:success="successLogin()" v-on:cancel="cancelLogin()"/>
   </div>
 </template>
 
 <script lang="ts">
+import axios from "axios";
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Signup from "@/components/Signup.vue";
 import Login from "@/components/Login.vue";
+import { APIConfig } from "@/utils/api.utils";
 
 @Component({
   components: {
@@ -45,20 +56,40 @@ export default class App extends Vue {
   showSignupModal() {
     this.showSignup = true;
   }
+
   successSignup() {
     this.showSignup = false;
   }
+
   cancelSignup() {
     this.showSignup = false;
   }
+
   showLoginModal() {
     this.showLogin = true;
   }
+
   successLogin() {
     this.showLogin = false;
   }
+
   cancelLogin() {
     this.showLogin = false;
+  }
+
+  get isLoggedIn(): boolean {
+    return !!this.$store.state.userId;
+  }
+
+  logout() {
+    debugger;
+    axios
+      .post(APIConfig.buildUrl("/logout"), null, {
+        headers: { token: this.$store.state.userToken }
+      })
+      .then(() => {
+        this.$store.commit("logout");
+      });
   }
 }
 </script>
