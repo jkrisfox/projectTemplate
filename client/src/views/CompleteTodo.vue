e9ad3d1ae63274df3db26343b92bf34d88011ee1
sangmin6810 <35266080+sangmin6810@users.noreply.github.com>
2019-02-06 19:32:51 -0800

    completion

---
+++ client/src/views/CompleteTodo.vue
@@ -0,0 +1,29 @@
+<template>
+    <span>
+        <button class ="completeButt" v-on:click="completeTodo(todo)">Complete</button>
+    </span>
+</template>
+
+<script>
+export default{
+    name: "CompleteTodo",
+    methods: {
+        completeTodo(todo){
+            this.$emit('complete-todo', todo);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .completeButt{
+        text-align: center;
+		border: none;
+  		border-radius: 4px;
+  		cursor: pointer;
+		color: white;
+		background-color: rgb(24, 114, 121);
+		font-size: 60%;
+		
+    }
+</style>
