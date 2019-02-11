7668404ad6c1e1586267c97b20d11ce2a1f83190
gwayne9955 <gwayne9955@yahoo.com>
2019-02-10 20:02:09 -0800

    End of work session, began add new product page

---
+++ client/src/models/item.interface.ts
@@ -0,0 +1,10 @@
+export interface iItem {
+    name: string;
+    description: string;
+    price: number;
+    imageUrls: string;
+    stockCount: number;
+    tags: string;
+    inStoreOnly: boolean;
+  }
+  
--- client/src/router.ts
+++ client/src/router.ts
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import ToDos from "./views/ToDos.vue";
 import MyProfile from "./views/MyProfile.vue";
+import NewProduct from "./views/NewProduct.vue";
 
 Vue.use(Router);
 
@@ -33,6 +34,20 @@ export default new Router({
       path: "/my-profile",
       name: "myProfile",
       component: MyProfile
+    },
+    {
+      path: "/products",
+      name: "products",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "products" */ "./views/Products.vue")
+    },
+    {
+      path: "/products/newproduct",
+      name: "newProduct",
+      component: NewProduct
     }
   ]
 });
