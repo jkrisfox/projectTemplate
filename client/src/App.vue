0caeede0bd942186583b2428a74ed2ba8be8f02a
gwayne9955 <gwayne9955@yahoo.com>
2019-02-10 17:22:06 -0800

    Added first draft of nav bar and logo

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="app">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <div class="navbar">
       <div class="navbar-menu">
+        <div class="navbar-logo">
+          <a href="/">
+            <img class="foxcycle-logo-img" src="./assets/foxcyclelogo.png">
+            <p class="foxcycle-logo-text">FoxCycle</p>
+          </a>
+        </div>
         <div class="navbar-start">
           <router-link class="navbar-item is-tab" to="/" exact-active-class="is-active">Home</router-link> 
+          <router-link class="navbar-item is-tab" to="/services" exact-active-class="is-active">Services</router-link>
+          <router-link class="navbar-item is-tab" to="/products" exact-active-class="is-active">Products</router-link>
           <router-link class="navbar-item is-tab" to="/about" exact-active-class="is-active">About</router-link>
         </div>
         <div class="navbar-end">
           <div class="navbar-item">
+            <span class="icon">
+              <router-link to="/cart" exact-active-class="is-active">
+                <i class="fas fa-shopping-cart"></i>
+              </router-link>
+            </span>
             <div class="buttons">
               <a class="button is-primary" v-on:click="showSignupModal()">
                 <strong>Sign up</strong>
@@ -78,4 +92,26 @@ export default class App extends Vue {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
+
+.foxcycle-logo-img {
+  height: 100px;
+  width: 100px;
+}
+
+.navbar-start {
+  margin-left: auto;
+  margin-right: unset;
+}
+
+.foxcycle-logo-text {
+  display: inline;
+  font-size: 2.5rem;
+  color: black;
+}
+
+.fas.fa-shopping-cart {
+  margin-right: 25px;
+  color: black;
+}
+
 </style>
