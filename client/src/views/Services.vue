c49f05fa3f9752dc72de3b437c9d87db69502d26
abhijeetsidhu <abhijeetsidhu@Abhijeets-MacBook-Pro.local>
2019-02-10 17:45:27 -0800

    services

---
+++ client/src/views/Services.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="services tile is-ancestor">
+    <h1>All services are performed in store</h1>
+        <div class="tile">
+        Service #1: 
+        Brake service 
+        </div>
+  </div>
+</template>
