fc045be1d46b01ec0e4a7132b05f59258178bfa9
Evan Zhang <evanlzhang@gmail.com>
2019-02-25 18:41:18 -0800

    mostly recovered

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,25 +1,25 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import ToDos from "./views/ToDos.vue";
+import MyProfile from "./views/MyProfile.vue";
+
+
 
 Vue.use(Router);
 
-@@ -21,6 +22,11 @@ export default new Router({
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
     },
     {
       path: "/my-profile",
       name: "myProfile",
       component: MyProfile
-    },
-    {
-      path: "/todos",
-      name: "todos",
-      component: ToDos
-
     }
   ]
 });
